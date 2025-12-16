e4a792f04121de47e5360a441c6e116f0e017950
JBatsyus <ju.kalabina@gmail.com>
2025-12-16 15:00:07 +0200

    секция спецпредложения десктоп

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 const heroSwiper = new Swiper('.hero-slider__swiper', {
     loop: true,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true
-        },
-        speed: 1000,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: true
-        },
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 1000,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true
+    },
     navigation: {
         prevEl: '.slider__nav--prev',
         nextEl: '.slider__nav--next',
@@ -18,4 +18,33 @@ const heroSwiper = new Swiper('.hero-slider__swiper', {
     //   el: '.swiper-pagination',
     //   clickable: true,
     // },
+});
+
+const specialOffersSwiper = new Swiper('.special-offers__swiper', {
+    loop: true,
+    speed: 600,
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    // Адаптивность
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+
+    navigation: {
+        prevEl: '.special-offers__nav--prev',
+        nextEl: '.special-offers__nav--next',
+    },
+
 });
